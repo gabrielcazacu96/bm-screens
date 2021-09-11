445fddcff4cc684a72ccbecb717c5443a98889dd
Cazacu Vlad <vlad.cazacu@cultofcoders.com>
2021-09-11 13:59:31 +0300

    react + storybook init

---
+++ src/components/Registration/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+interface Props {
+
+}
+
+const Registration: React.FC<Props> = ({ }) => {
+
+    return (
+        <div></div>
+    );
+};
+
+export default Registration;
---
+++ src/stories/Registration.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Registration } from '../components';
+
+export default {
+  title: 'Screens/Registration',
+  component: Registration,
+} as ComponentMeta<typeof Registration>;
+
+const Template: ComponentStory<typeof Registration> = (args) => <Registration {...args} />;
+
+export const RegistrationSimple = Template.bind({});
+// LoggedIn.args = {
+//   ...HeaderStories.LoggedIn.args,
+// };
